7c0bcfc6115953d55a31cd5e696403a726cfc8d0
Tristan Leid <77852358+crowbar72@users.noreply.github.com>
2022-10-03 08:09:07 -0400

    Update auth.py

--- App/controllers/auth.py
+++ App/controllers/auth.py
@@ -1,24 +1,31 @@
-import flask_login
-from flask_jwt import JWT
-from App.models import User
+from werkzeug.security import check_password_hash, generate_password_hash
+from App.database import db
+from flask import jsonify
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username =  db.Column(db.String, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    images = db.relationship('Image', backref='image', lazy=True, cascade="all, delete-orphan")
+    ratings = db.relationship('Rating', backref='rating', lazy=True, cascade="all, delete-orphan")
 
-def authenticate(username, password):
-    user = User.query.filter_by(username=username).first()
-    if user and user.check_password(password):
-        return user
-    return None
+    def __init__(self, username, password):
+        self.username = username
+        self.set_password(password)
 
-# Payload is a dictionary which is passed to the function by Flask JWT
-def identity(payload):
-    return User.query.get(payload['identity'])
+    def toJSON(self):
+        return{
+            'id': self.id,
+            'username': self.username,
+            'images': [image.toJSON() for image in self.images],
+            'ratings': [rating.toJSON() for rating in self.ratings]
+        }
+    
 
-def login_user(user, remember):
-    return flask_login.login_user(user, remember=remember)
-
-
-def logout_user():
-    flask_login.logout_user()
-
-def setup_jwt(app):
-    return JWT(app, authenticate, identity)
+    def set_password(self, password):
+        """Create hashed password."""
+        self.password = generate_password_hash(password, method='sha256')
+    
+    def check_password(self, password):
+        """Check hashed password."""
+        return check_password_hash(self.password, password)
