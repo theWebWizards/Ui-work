f84634a4378022dbd872d63b13d5286bfce90e7f
crowbar72 <tristanleid72@gmail.com>
2022-11-04 21:18:09 +0000

    commit

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -29,24 +29,24 @@ class UserUnitTests(unittest.TestCase):
 
     # pure function no side effects or integrations called
     def test_toJSON(self):
-        user = User("adam", "adampass")
+        user = User("bob", "bobpass")
         user_json = user.toJSON()
-        self.assertDictEqual(user_json, {"id":None, "username":"adam", "images": [], "ratings": []})
+        self.assertDictEqual(user_json, {"id":None, "username":"bob", "images": [], "ratings": []})
     
-    def test_authenticate():
-        user = create_user("eve", "evepass")
-        assert authenticate("eve", "evepass") != None
-
     def test_hashed_password(self):
         password = "mypass"
         hashed = generate_password_hash(password, method='sha256')
-        user = User("lilly", password)
+        user = User("bob", password)
         assert user.password != password
 
     def test_check_password(self):
         password = "mypass"
-        user = User("scott", password)
+        user = User("bob", password)
         assert user.check_password(password)
+
+    # def test_get_user_by_username(self):
+    #     user = User("bob", "bobpass")
+    #     assert get_user_by_username("bob")
 
 '''
     Integration Tests
@@ -54,22 +54,25 @@ class UserUnitTests(unittest.TestCase):
 
 # This fixture creates an empty database for the test and deletes it after the test
 # scope="class" would execute the fixture once and resued for all methods in the class
-@pytest.fixture(autouse=True, scope="class")
+@pytest.fixture(autouse=True, scope="module")
 def empty_db():
     app.config.update({'TESTING': True, 'SQLALCHEMY_DATABASE_URI': 'sqlite:///test.db'})
     create_db(app)
     yield app.test_client()
     os.unlink(os.getcwd()+'/App/test.db')
 
+def test_authenticate():
+        user = create_user("bob", "bobpass")
+        assert authenticate("bob", "bobpass") != None
+
 
 class UsersIntegrationTests(unittest.TestCase):
 
     def test_create_user(self):
-        user = create_user("bob", "bobpass")
-        assert user.username == "bob"
+        user = create_user("rick", "bobpass")
+        assert user.username == "rick"
 
     def test_get_all_users_json(self):
-        user = create_user("rick", "bobpass")
         users_json = get_all_users_json()
         self.assertListEqual([{"id":1, "username":"bob", "images": [], "ratings": []}, {"id":2, "username":"rick", "images": [], "ratings": []}], users_json)
 
